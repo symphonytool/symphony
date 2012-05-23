271ba065369c88d1446423b611037456dcb2f6f4
Rasmus Lauritsen <rwl@Engi.(none)>
2012-05-23 10:51:08 +0200

    stuff

--- core/parser/run.sh
+++ core/parser/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#
+# Usage: ./run.sh <example-name>
+#
+# E.g. ./run.sh types.cml will run the parser on types.cml
+# from the ../../docs/cml-examples directory.
+#
 CLASSPATH=target:.:../ast/target/ast-0.0.1.jar:target/parser-0.0.1.jar
 java -cp ${CLASSPATH} eu.compassresearch.core.parser.CmlParser  ../../docs/cml-examples/$1
