4ff8b9cf0c8168e2dd56651af66ffc6b68192a96
Rasmus <rala@iha.dk>
2012-05-25 14:31:28 +0200

    Updated script to print colors :)

--- core/parser/runall.sh
+++ core/parser/runall.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 #Who am I ?
 #I am Who.
-`cat README.bison`
-mvn install
+red=31
+green=32
+yellow=33
+blue=34
+width=70
+function colortxt()
+{
+    color=$1
+    txt=$2
+    echo -e "\e[1;${color}m${txt}\e[0m"
+};
+#`cat README.bison`
+#mvn install
  for test in $(ls -1 ../../docs/cml-examples/); do 
-     echo -n ${test};
-     echo -n -e "\t\t\t\t";
+     txt=$(echo -n ${test});
+     notabs=$(expr ${width} - ${#txt} );
+     str=$(printf "%-${notabs}s" " ");
+     tabstr=$(echo -n -e "${str// / }\t");
      ./run.sh ${test} 1>/dev/null  2>/dev/null;
      retcode=$?;
      if [ "${retcode}" == "0" ]; 
      then 
-	 echo "[ok]"; 
+	 echo -n -e "$(colortxt ${green} ${txt}) ${tabstr}";
+	 echo $(colortxt ${green} "[ok]"); 
      elif [ "${retcode}" == "255" ]; then
-	 echo "[not parsing]";
+	 echo -n -e "$(colortxt ${yellow} ${txt}) ${tabstr}";
+	 echo $(colortxt ${yellow} "[not parsing]");
      elif [ "${retcode}" == "254" ]; then
-	 echo "[file not found]";
+	 echo -n -e "$(colortxt ${yellow} ${txt}) ${tabstr}";
+	 echo $(colortxt ${blue} "[file not found]");
      elif [ "${retcode}" == "253" ]; then
-	 echo "[IO Error]";
+	 echo -n -e "$(colortxt ${yellow} ${txt}) ${tabstr}";
+	 echo $(colortxt ${red} "[IO Error]");
      elif [ "${retcode}" == "252" ]; then
-	 echo "[Unexpected error]"; 
+	 echo -n -e "$(colortxt ${yellow} ${txt}) ${tabstr}";
+	 echo $(colortxt ${red} "[Unexpected error]"); 
      else
+	 echo -n -e "$(colortxt ${red} ${txt}) ${tabstr}";
 	 echo -e "[What Bitches] ${retcode}";
      fi
      
