93ea906a7f667c29ccf0199ac5d32c703a724765
Rasmus <rala@iha.dk>
2012-07-10 19:10:03 +0200

    Added Equinox experiments

---
+++ equinox/cmlast/src/main/java/eu/compassresearch/equinox/ast/AstActivator.java
@@ -0,0 +1,9 @@
+package eu.compassresearch.equinox.ast;
+
+import org.osgi.framework.*;
+
+public class AstActivator implements BundleActivator {
+
+    public void start(BundleContext bundle) {}
+    public void stop (BundleContext bundle) {}
+}
