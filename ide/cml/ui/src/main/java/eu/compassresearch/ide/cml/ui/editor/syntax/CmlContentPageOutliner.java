dc9f709accf782b2707c083c90e5530701e000de
Rasmus Winther Lauritsen (rale@iha.dk) <rala@iha.dk>
2012-09-07 10:03:23 +0200

    Headless build working:  *) COMPASS executable starts  *) We have Outliner  *) Type Checking invoked deps to Overture-TC fixed  *) Refactored deps, all dependencies on core are through the core-plugin  *) Build scripts for build different platforms (win, mac, linux) added

--- ide/cml/ui/src/main/java/eu/compassresearch/ide/cml/ui/editor/syntax/CmlContentPageOutliner.java
+++ ide/cml/ui/src/main/java/eu/compassresearch/ide/cml/ui/editor/syntax/CmlContentPageOutliner.java
@@ -41,7 +41,7 @@ public class CmlContentPageOutliner extends ContentOutlinePage implements
         super.createControl(parent);
         TreeViewer viewer = getTreeViewer();
         viewer.setContentProvider(provider);
-        viewer.setLabelProvider(new CmlLabelProvider());
+        // viewer.setLabelProvider(new CmlLabelProvider());
         viewer.addSelectionChangedListener(this);
         viewer.setInput(input);
       }
