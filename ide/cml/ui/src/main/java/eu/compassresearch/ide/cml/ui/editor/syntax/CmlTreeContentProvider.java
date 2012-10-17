7156e2b2267275ab708dd2c3accec7cc42b57bc8
Luis Duarte Couto <lcouto@iha.dk>
2012-10-17 15:58:11 +0200

    Values parent extracted from outline. Code a bit messy.

--- ide/cml/ui/src/main/java/eu/compassresearch/ide/cml/ui/editor/syntax/CmlTreeContentProvider.java
+++ ide/cml/ui/src/main/java/eu/compassresearch/ide/cml/ui/editor/syntax/CmlTreeContentProvider.java
@@ -21,6 +21,7 @@ import eu.compassresearch.ast.definitions.AFunctionParagraphDefinition;
 import eu.compassresearch.ast.definitions.AOperationParagraphDefinition;
 import eu.compassresearch.ast.definitions.AProcessDefinition;
 import eu.compassresearch.ast.definitions.AProcessParagraphDefinition;
+import eu.compassresearch.ast.definitions.ATypesParagraphDefinition;
 import eu.compassresearch.ast.definitions.AValueParagraphDefinition;
 import eu.compassresearch.ast.definitions.PDefinition;
 import eu.compassresearch.ast.definitions.SFunctionDefinition;
@@ -32,279 +33,264 @@ import eu.compassresearch.ast.process.PProcess;
 import eu.compassresearch.ast.program.PSource;
 import eu.compassresearch.ide.cml.ui.editor.core.dom.CmlSourceUnit;
 
-public class CmlTreeContentProvider implements ITreeContentProvider
-  {
-    
-    private final Control parentControl;
-    
-    public CmlTreeContentProvider(Control control)
-      {
-        parentControl = control;
-      }
-    
-    @Override
-    public void dispose()
-      {
+public class CmlTreeContentProvider implements ITreeContentProvider {
+
+	private final Control parentControl;
+
+	public CmlTreeContentProvider(Control control) {
+		parentControl = control;
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+	}
+
+	private PSource current;
+
+	@Override
+	public Object[] getElements(Object inputElement) {
+		if (inputElement instanceof CmlSourceUnit) {
+			// Get current source tree
+			current = ((CmlSourceUnit) inputElement).getSourceAst();
+			if (current == null)
+				return new Object[0];
+
+			// If there are any declarations lets see them
+			List<Wrapper<PDefinition>> res = new LinkedList<CmlTreeContentProvider.Wrapper<PDefinition>>();
+			for (PDefinition def : current.getParagraphs())
+				res.add(Wrapper.newInstance(def, def.getName().name));
+			return res.toArray();
+		}
+		return new Object[0];
+	}
+
+	private static class Wrapper<T> extends FieldDecoration {
+		private String str;
+
+		public static <T> Wrapper<T> newInstance(T value, String str) {
+			return new Wrapper<T>(null, value, str);
+		}
+
+		public Wrapper(Image img, T value, String str) {
+			super(img, str);
+			this.value = value;
+			this.str = str;
+		}
+
+		public T value;
+
+		public boolean isClass(Class<?> clz) {
+			return clz.isInstance(value);
+		}
+
+		public String toString() {
+			return str;
+		}
+
+	}
+
+	@Override
+	public Object[] getChildren(Object parentElement) {
+
+		Object n = parentElement;
+
+		if (n instanceof Wrapper) {
+			Wrapper w = (Wrapper) n;
+
+			if (w.isClass(AClassParagraphDefinition.class)) {
+				List<Wrapper<PDefinition>> res = new LinkedList<Wrapper<PDefinition>>();
+				AClassParagraphDefinition clzdecl = (AClassParagraphDefinition) w.value;
+				for (PDefinition section : clzdecl.getDefinitions()) {
+					res.addAll(extractChildren(section));
+					/* we need to split these by definition types. Only the drilldowns should 
+					have children extracted. the rest go normal name extraction*/
+					res.add(extractPDefinitionName(section));
+				}
+				return res.toArray();
+			}
+
+			if (w.isClass(AValueParagraphDefinition.class)) {
+				return handleValueParagraphDefinition(
+						(AValueParagraphDefinition) w.value).toArray();
+			}
+
+			if (w.isClass(AProcessParagraphDefinition.class)
+					|| w.isClass(PProcess.class)) {
+				AProcessParagraphDefinition processDecl = (AProcessParagraphDefinition) w.value;
+				AProcessDefinition def = processDecl.getProcessDefinition();
+				PProcess process = def.getProcess();
+
+				List<Object> res = handleProcess(process);
+
+				return res.toArray();
+			}
+
+			if (w.isClass(ASequentialCompositionProcess.class)) {
+				List<Object> res = new LinkedList<Object>();
+				ASequentialCompositionProcess comp = (ASequentialCompositionProcess) w.value;
+				handleSequentialCompisitionProcess(res, comp);
+				return res.toArray();
+			}
+
+			if (((Wrapper) n).isClass(AFunctionParagraphDefinition.class)) {
+				List<String> res = new LinkedList<String>();
+				AFunctionParagraphDefinition fd = (AFunctionParagraphDefinition) w.value;
+				for (SFunctionDefinition fnd : fd.getFunctionDefinitions()) {
+					res.add("" + notNullName(fnd.getName()) + ":"
+							+ fnd.getType());
+				}
+				return res.toArray();
+			}
+
+			if (((Wrapper) n).isClass(AOperationParagraphDefinition.class)) {
+				List<String> res = new LinkedList<String>();
+				AOperationParagraphDefinition od = (AOperationParagraphDefinition) w.value;
+				for (SOperationDefinition sod : od.getOperations()) {
+					res.add("[O] " + notNullName(sod.getName()));
+				}
+				return res.toArray();
+			}
+
+			if (((Wrapper) n).isClass(AActionParagraphDefinition.class)) {
+				List<String> res = new LinkedList<String>();
+				AActionParagraphDefinition ad = (AActionParagraphDefinition) w.value;
+				for (AActionDefinition a : ad.getActions()) {
+					res.add("[A] " + notNullName(a.getName()));
+				}
+				return res.toArray();
+
+			}
+		}
+
+		return new String[0];
+	}
+
+	private List<Wrapper<PDefinition>> extractChildren(PDefinition pdef) {
+		List<Wrapper<PDefinition>> res = new LinkedList<Wrapper<PDefinition>>();
+		
+		if (pdef instanceof AValueParagraphDefinition){
+			for (PDefinition subdef : ((AValueParagraphDefinition)pdef).getValueDefinitions())
+			{
+				res.add(Wrapper.newInstance(subdef, subdef.getName().name + "->" + subdef.getType()));
+			}
+		}
+	
+		return res;
+	}
+
+	private Wrapper<PDefinition> extractPDefinitionName(PDefinition pdef){	
+        if (pdef instanceof AValueParagraphDefinition)
+        	return Wrapper.newInstance(pdef, "Values");
+    	if (pdef instanceof AFunctionParagraphDefinition)
+    		return Wrapper.newInstance(pdef, "Functions");
+        if (pdef instanceof AOperationParagraphDefinition)
+    		return Wrapper.newInstance(pdef, "Operations");
+        if (pdef instanceof ATypesParagraphDefinition)
+    		return Wrapper.newInstance(pdef, "Types");
+        return Wrapper.newInstance(pdef, "ERROR: unknown section");
         
-      }
-    
-    @Override
-    public void inputChanged(Viewer viewer, Object oldInput, Object newInput)
-      {
-      }
-    
-    private PSource current;
-    
-    @Override
-    public Object[] getElements(Object inputElement)
-      {
-        if (inputElement instanceof CmlSourceUnit)
-          {
-            // Get current source tree
-            current = ((CmlSourceUnit) inputElement).getSourceAst();
-            if (current == null)
-              return new Object[0];
-            
-            // If there are any declarations lets see them
-            List<Wrapper<PDefinition>> res = new LinkedList<CmlTreeContentProvider.Wrapper<PDefinition>>();
-            for (PDefinition def : current.getParagraphs())
-              res.add(Wrapper.newInstance(def, def.getName().name));
-            return res.toArray();
-          }
-        return new Object[0];
-      }
-    
-    private static class Wrapper<T> extends FieldDecoration
-      {
-        private String str;
-        
-        public static <T> Wrapper<T> newInstance(T value, String str)
-          {
-            return new Wrapper<T>(null, value, str);
-          }
-        
-        public Wrapper(Image img, T value, String str)
-          {
-            super(img, str);
-            this.value = value;
-            this.str = str;
-          }
-        
-        public T value;
-        
-        public boolean isClass(Class<?> clz)
-          {
-            return clz.isInstance(value);
-          }
-        
-        public String toString()
-          {
-            return str;
-          }
-        
-      }
-    
-    @Override
-    public Object[] getChildren(Object parentElement)
-      {
-        
-        Object n = parentElement;
-        
-        if (n instanceof Wrapper)
-          {
-            Wrapper w = (Wrapper) n;
-            
-            if (w.isClass(AClassParagraphDefinition.class))
-              {
-                List<Wrapper<PDefinition>> res = new LinkedList<Wrapper<PDefinition>>();
-                AClassParagraphDefinition clzdecl = (AClassParagraphDefinition) w.value;
-                for (PDefinition section : clzdecl.getDefinitions())
-                  {
-                    if (section instanceof AValueParagraphDefinition)
-                      res.add(Wrapper.newInstance(section, "Values"));
-                    else if (section instanceof AFunctionParagraphDefinition)
-                      res.add(Wrapper.newInstance(section, "Functions"));
-                    else if (section instanceof AOperationParagraphDefinition)
-                      res.add(Wrapper.newInstance(section, "Operations"));
-                    else
-                      res.add(Wrapper.newInstance(section,
-                          section.getName().name));
-                  }
-                return res.toArray();
-              }
-            
-            if (w.isClass(AValueParagraphDefinition.class))
-              {
-                return handleValueParagraphDefinition(
-                    (AValueParagraphDefinition) w.value).toArray();
-              }
-            
-            if (w.isClass(AProcessParagraphDefinition.class)
-                || w.isClass(PProcess.class))
-              {
-                AProcessParagraphDefinition processDecl = (AProcessParagraphDefinition) w.value;
-                AProcessDefinition def = processDecl.getProcessDefinition();
-                PProcess process = def.getProcess();
-                
-                List<Object> res = handleProcess(process);
-                
-                return res.toArray();
-              }
-            
-            if (w.isClass(ASequentialCompositionProcess.class))
-              {
-                List<Object> res = new LinkedList<Object>();
-                ASequentialCompositionProcess comp = (ASequentialCompositionProcess) w.value;
-                handleSequentialCompisitionProcess(res, comp);
-                return res.toArray();
-              }
-            
-            if (((Wrapper) n).isClass(AFunctionParagraphDefinition.class))
-              {
-                List<String> res = new LinkedList<String>();
-                AFunctionParagraphDefinition fd = (AFunctionParagraphDefinition) w.value;
-                for (SFunctionDefinition fnd : fd.getFunctionDefinitions())
-                  {
-                    res.add("" + notNullName(fnd.getName()) + ":"
-                        + fnd.getType());
-                  }
-                return res.toArray();
-              }
-            
-            if (((Wrapper) n).isClass(AOperationParagraphDefinition.class))
-              {
-                List<String> res = new LinkedList<String>();
-                AOperationParagraphDefinition od = (AOperationParagraphDefinition) w.value;
-                for (SOperationDefinition sod : od.getOperations())
-                  {
-                    res.add("[O] " + notNullName(sod.getName()));
-                  }
-                return res.toArray();
-              }
-            
-            if (((Wrapper) n).isClass(AActionParagraphDefinition.class))
-              {
-                List<String> res = new LinkedList<String>();
-                AActionParagraphDefinition ad = (AActionParagraphDefinition) w.value;
-                for (AActionDefinition a : ad.getActions())
-                  {
-                    res.add("[A] " + notNullName(a.getName()));
-                  }
-                return res.toArray();
-                
-              }
-          }
-        
-        return new String[0];
-      }
-    
-    private List<Object> handleProcess(PProcess process)
-      {
-        List<Object> res = new LinkedList<Object>();
-        if (process instanceof AStateProcess)
-          {
-            AStateProcess sp = (AStateProcess) process;
-            PAction a = sp.getAction();
-            addActions(res, a);
-            return res;
-          }
-        
-        if (process instanceof ASequentialCompositionProcess)
-          {
-            ASequentialCompositionProcess p = (ASequentialCompositionProcess) process;
-            PProcess left = p.getLeft();
-            PProcess right = p.getRight();
-            res.add(Wrapper.newInstance(left, "left"));
-            res.add(Wrapper.newInstance(right, "right"));
-            return res;
-          }
-        
-        res.add("Not implemented yet \"" + process.getClass().getName() + "\"");
-        return res;
-      }
-    
-    private void handleSequentialCompisitionProcess(List<Object> res,
-        ASequentialCompositionProcess comp)
-      {
-        PProcess left = comp.getLeft();
-        PProcess right = comp.getRight();
-        
-        if (left instanceof ASequentialCompositionProcess)
-          handleSequentialCompisitionProcess(res,
-              (ASequentialCompositionProcess) left);
-        else
-          handleOtherProcess(res, left);
-        
-        if (right instanceof ASequentialCompositionProcess)
-          handleSequentialCompisitionProcess(res,
-              (ASequentialCompositionProcess) right);
-        else
-          handleOtherProcess(res, right);
-      }
-    
-    private void handleOtherProcess(List<Object> res, PProcess p)
-      {
-        if (p instanceof AStateProcess)
-          res.add("State Process");
-        else
-          res.add("** Process **");
-      }
-    
-    private void addActions(List<Object> res, PAction a)
-      {
-        if (a instanceof ASkipAction)
-          {
-            res.add("Skip");
-            return;
-          }
-        if (a instanceof AStopAction)
-          {
-            res.add("Stop");
-            return;
-          }
-        if (a instanceof AChaosAction)
-          {
-            res.add("Chaos");
-            return;
-          }
-        if (a instanceof ASequentialCompositionAction)
-          {
-            ASequentialCompositionAction seqa = (ASequentialCompositionAction) a;
-            PAction left = seqa.getLeft();
-            PAction right = seqa.getRight();
-            addActions(res, left);
-            addActions(res, right);
-            return;
-          }
-      }
-    
-    private List<String> handleValueParagraphDefinition(
-        AValueParagraphDefinition cast)
-      {
-        List<String> res = new LinkedList<String>();
-        LinkedList<PDefinition> valDefs = cast.getValueDefinitions();
-        for (PDefinition def : valDefs)
-          res.add(def.getName().name + ":" + def.getType());
-        return res;
-      }
-    
-    private static String notNullName(LexIdentifierToken name)
-      {
-        if (name == null)
-          return "null";
-        return name.name;
-      }
-    
-    @Override
-    public Object getParent(Object element)
-      {
-        return null;
-      }
-    
-    @Override
-    public boolean hasChildren(Object element)
-      {
-        return element instanceof Wrapper;
-      }
-    
-  }
+	}
+
+
+	private List<Object> handleProcess(PProcess process) {
+		List<Object> res = new LinkedList<Object>();
+		if (process instanceof AStateProcess) {
+			AStateProcess sp = (AStateProcess) process;
+			PAction a = sp.getAction();
+			addActions(res, a);
+			return res;
+		}
+
+		if (process instanceof ASequentialCompositionProcess) {
+			ASequentialCompositionProcess p = (ASequentialCompositionProcess) process;
+			PProcess left = p.getLeft();
+			PProcess right = p.getRight();
+			res.add(Wrapper.newInstance(left, "left"));
+			res.add(Wrapper.newInstance(right, "right"));
+			return res;
+		}
+
+		res.add("Not implemented yet \"" + process.getClass().getName() + "\"");
+		return res;
+	}
+
+	private void handleSequentialCompisitionProcess(List<Object> res,
+			ASequentialCompositionProcess comp) {
+		PProcess left = comp.getLeft();
+		PProcess right = comp.getRight();
+
+		if (left instanceof ASequentialCompositionProcess)
+			handleSequentialCompisitionProcess(res,
+					(ASequentialCompositionProcess) left);
+		else
+			handleOtherProcess(res, left);
+
+		if (right instanceof ASequentialCompositionProcess)
+			handleSequentialCompisitionProcess(res,
+					(ASequentialCompositionProcess) right);
+		else
+			handleOtherProcess(res, right);
+	}
+
+	private void handleOtherProcess(List<Object> res, PProcess p) {
+		if (p instanceof AStateProcess)
+			res.add("State Process");
+		else
+			res.add("** Process **");
+	}
+
+	private void addActions(List<Object> res, PAction a) {
+		if (a instanceof ASkipAction) {
+			res.add("Skip");
+			return;
+		}
+		if (a instanceof AStopAction) {
+			res.add("Stop");
+			return;
+		}
+		if (a instanceof AChaosAction) {
+			res.add("Chaos");
+			return;
+		}
+		if (a instanceof ASequentialCompositionAction) {
+			ASequentialCompositionAction seqa = (ASequentialCompositionAction) a;
+			PAction left = seqa.getLeft();
+			PAction right = seqa.getRight();
+			addActions(res, left);
+			addActions(res, right);
+			return;
+		}
+	}
+
+	private List<String> handleValueParagraphDefinition(
+			AValueParagraphDefinition cast) {
+		List<String> res = new LinkedList<String>();
+		LinkedList<PDefinition> valDefs = cast.getValueDefinitions();
+		for (PDefinition def : valDefs)
+			res.add(def.getName().name + "->" + def.getType());
+		return res;
+	}
+
+	private static String notNullName(LexIdentifierToken name) {
+		if (name == null)
+			return "null";
+		return name.name;
+	}
+	
+	// Why does this return null?
+	@Override
+	public Object getParent(Object element) {
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren(Object element) {
+		return element instanceof Wrapper;
+	}
+
+}
