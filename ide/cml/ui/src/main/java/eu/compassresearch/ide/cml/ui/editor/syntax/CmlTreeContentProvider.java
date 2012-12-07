f6e4c8682cdd0676e70ea2f68a6013b65a05ada9
Luis D Couto <lcouto@iha.dk>
2012-12-07 10:33:53 +0100

    Added outline-editor sync.
    There's some weird behaviour when selecting inner definitions. The outline needs to be opened manually before if syncs on those.
    
    When we load the model at startup the expand-all should fix that.

--- ide/cml/ui/src/main/java/eu/compassresearch/ide/cml/ui/editor/syntax/CmlTreeContentProvider.java
+++ ide/cml/ui/src/main/java/eu/compassresearch/ide/cml/ui/editor/syntax/CmlTreeContentProvider.java
@@ -26,185 +26,187 @@ import eu.compassresearch.ide.cml.ui.editor.core.dom.CmlSourceUnit;
 
 public class CmlTreeContentProvider implements ITreeContentProvider {
 
-	private static final String SMILING_ERROR_STRING = "P = NP ? Well we are working on it.";
-	private final Control parentControl;
+    private static final String SMILING_ERROR_STRING = "P = NP ? Well we are working on it.";
+    private final Control parentControl;
 
-	public CmlTreeContentProvider(Control control) {
-		parentControl = control;
-	}
+    public CmlTreeContentProvider(Control control) {
+	parentControl = control;
+    }
 
-	@Override
-	public void dispose() {
+    @Override
+    public void dispose() {
 
-	}
+    }
 
-	@Override
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-	}
+    @Override
+    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+    }
 
-	private PSource current;
+    private PSource current;
 
-	@Override
-	public Object[] getElements(Object inputElement) {
+    @Override
+    public Object[] getElements(Object inputElement) {
 
-		try {
-			if (inputElement instanceof CmlSourceUnit) {
-				// Get current source tree
-				current = ((CmlSourceUnit) inputElement).getSourceAst();
-				if (current == null)
-					return new Object[0];
-				// If there are any declarations lets see them
-				List<Wrapper<PDefinition>> res = new LinkedList<Wrapper<PDefinition>>();
-				for (PDefinition def : current.getParagraphs()) {
+	try {
+	    if (inputElement instanceof CmlSourceUnit) {
+		// Get current source tree
+		current = ((CmlSourceUnit) inputElement).getSourceAst();
+		if (current == null)
+		    return new Object[0];
+		// If there are any declarations lets see them
+		List<Wrapper<PDefinition>> res = new LinkedList<Wrapper<PDefinition>>();
+		for (PDefinition def : current.getParagraphs()) {
 
-					// Get the entry names for the global declarations
-					String dscr = TopLevelDefinitionMap.getDescription(def
-							.getClass());
-					if (dscr == null)
-						res.add(Wrapper.newInstance(def, def.getName().name));
-					else
-						res.add(Wrapper.newInstance(def, dscr));
+		    // Get the entry names for the global declarations
+		    String dscr = TopLevelDefinitionMap.getDescription(def
+			    .getClass());
+		    if (dscr == null)
+			res.add(Wrapper.newInstance(def, def.getName().name));
+		    else
+			res.add(Wrapper.newInstance(def, dscr));
 
-				}
-				return res.toArray();
-			}
-		} catch (Exception e) {
-			return new String[] { SMILING_ERROR_STRING };
 		}
-		return new String[0];
-	}
-
-	@Override
-	public Object[] getChildren(Object parentElement) {
-		try {
-			Object n = parentElement;
-
-			if (n instanceof Wrapper) {
-				Wrapper w = (Wrapper) n;
-
-			
-				if (w.isClass(AClassParagraphDefinition.class)) {
-					return handleClassParagraphDefinition(
-							(AClassParagraphDefinition) w.value).toArray();
-				}
-
-				if (w.isClass(AChannelParagraphDefinition.class))
-					return handleChannelParagraphDefinition(
-							(AChannelParagraphDefinition) w.value).toArray();
-
-				if (w.isClass(AChansetParagraphDefinition.class))
-					return handleChansetParagraphDefinition(
-							(AChansetParagraphDefinition) w.value).toArray();
-
-				if (w.isClass(AValueParagraphDefinition.class)) {
-					return handleValueParagraphDefinition(
-							(AValueParagraphDefinition) w.value).toArray();
-				}
-
-				if (w.isClass(AProcessParagraphDefinition.class)) {
-					return handleProcessParagraphDefinition(
-							(AProcessParagraphDefinition) w.value).toArray();
-				}
-				
-				if (w.isClass(AStateProcess.class)) {
-				    return handleReferenceProcess((AStateProcess) w.value).toArray();
-				}
-				
-
-				if (w.isClass(ATypesParagraphDefinition.class)) {
-					List<Wrapper<? extends PDefinition>> res = new LinkedList<Wrapper<? extends PDefinition>>();
-					ATypesParagraphDefinition td = (ATypesParagraphDefinition) w.value;
-					res = DefinitionMap.getDelegate(td.getClass())
-							.extractSubdefinition(td);
-					return res.toArray();
-				}
-
-				if (w.isClass(AFunctionParagraphDefinition.class)) {
-					List<Wrapper<? extends PDefinition>> res = new LinkedList<Wrapper<? extends PDefinition>>();
-					AFunctionParagraphDefinition fd = (AFunctionParagraphDefinition) w.value;
-					res = DefinitionMap.getDelegate(fd.getClass())
-							.extractSubdefinition(fd);
-					return res.toArray();
-				}
-			}
-			return new String[0];
-		} catch (Exception e) {
-			return new String[] { SMILING_ERROR_STRING };
-		}
-	}
-
-
-
-	private List<Wrapper<? extends PDefinition>> handleChansetParagraphDefinition(
-			AChansetParagraphDefinition cspdef) {
-		List<Wrapper<? extends PDefinition>> r = new LinkedList<Wrapper<? extends PDefinition>>();
-		for (AChansetDefinition cdef : cspdef.getChansets()) {
-			r.add(Wrapper.newInstance(cdef, cdef.getIdentifier().toString()));
-		}
-		return r;
-	}
-
-	private List<Wrapper<? extends PDefinition>> handleClassParagraphDefinition(
-			AClassParagraphDefinition cpdef) {
-		List<Wrapper<? extends PDefinition>> r = new LinkedList<Wrapper<? extends PDefinition>>();
-		for (PDefinition pdef : cpdef.getDefinitions()) {
-			r.addAll(DefinitionMap.getDelegate(pdef.getClass())
-					.extractSubdefinition(pdef));
-		}
-		return r;
-	}
-
-	private List<Wrapper<? extends PDefinition>> handleValueParagraphDefinition(
-			AValueParagraphDefinition cast) {
-		return DefinitionMap.getDelegate(cast.getClass()).extractSubdefinition(
-				cast);
-	}
-
-	@Override
-	public Object getParent(Object element) {
-		if (element instanceof INode)
-			return ((INode) element).parent();
-		return null;
-	}
-
-	//TODO - correct hasChildren method for reference processes at the highest level
-	@Override
-	public boolean hasChildren(Object element) {
-	    if (element instanceof Wrapper){
-		Wrapper w = (Wrapper) element;
-		if (w.value instanceof AReferenceProcess)
-		    return false;
-		return true;
+		return res.toArray();
 	    }
-
-
-		return false;
+	} catch (Exception e) {
+	    return new String[] { SMILING_ERROR_STRING };
 	}
+	return new String[0];
+    }
 
-	private List<Wrapper<? extends INode>> handleReferenceProcess(
-		PProcess pp) {
-	    if (ProcessMap.getDelegate(pp.getClass()) != null)
-		return ProcessMap.getDelegate(pp.getClass()).makeEntries(pp);
-	    return new LinkedList<Wrapper<? extends INode>>();
-	}
-	
-	@SuppressWarnings("unchecked")
-	private List<Wrapper<? extends INode>> handleProcessParagraphDefinition(
-			AProcessParagraphDefinition ppdef) {
-		PProcess pp = ppdef.getProcessDefinition().getProcess();
-		if (ProcessMap.getDelegate(pp.getClass()) != null)
-			return ProcessMap.getDelegate(pp.getClass()).makeEntries(pp);
-		return new LinkedList<Wrapper<? extends INode>>();
-	}
+    @Override
+    public Object[] getChildren(Object parentElement) {
+	try {
+	    Object n = parentElement;
 
-	private List<Wrapper<AChannelNameDefinition>> handleChannelParagraphDefinition(
-			AChannelParagraphDefinition cpdef) {
-		List<Wrapper<AChannelNameDefinition>> r = new LinkedList<Wrapper<AChannelNameDefinition>>();
-		for (AChannelNameDefinition dec : cpdef.getChannelNameDeclarations()) {
-			r.add(Wrapper.newInstance(dec, dec.getSingleType().getIdentifiers()
-					.toString()
-					+ ": " + dec.getSingleType().getType()));
+	    if (n instanceof Wrapper) {
+		Wrapper w = (Wrapper) n;
+
+		if (w.isClass(AClassParagraphDefinition.class)) {
+		    return handleClassParagraphDefinition(
+			    (AClassParagraphDefinition) w.value).toArray();
 		}
-		return r;
+
+		if (w.isClass(AChannelParagraphDefinition.class))
+		    return handleChannelParagraphDefinition(
+			    (AChannelParagraphDefinition) w.value).toArray();
+
+		if (w.isClass(AChansetParagraphDefinition.class))
+		    return handleChansetParagraphDefinition(
+			    (AChansetParagraphDefinition) w.value).toArray();
+
+		if (w.isClass(AValueParagraphDefinition.class)) {
+		    return handleValueParagraphDefinition(
+			    (AValueParagraphDefinition) w.value).toArray();
+		}
+
+		if (w.isClass(AProcessParagraphDefinition.class)) {
+		    return handleProcessParagraphDefinition(
+			    (AProcessParagraphDefinition) w.value).toArray();
+		}
+
+		if (w.isClass(AStateProcess.class)) {
+		    return handleReferenceProcess((AStateProcess) w.value)
+			    .toArray();
+		}
+
+		if (w.isClass(ATypesParagraphDefinition.class)) {
+		    List<Wrapper<? extends PDefinition>> res = new LinkedList<Wrapper<? extends PDefinition>>();
+		    ATypesParagraphDefinition td = (ATypesParagraphDefinition) w.value;
+		    res = DefinitionMap.getDelegate(td.getClass())
+			    .extractSubdefinition(td);
+		    return res.toArray();
+		}
+
+		if (w.isClass(AFunctionParagraphDefinition.class)) {
+		    List<Wrapper<? extends PDefinition>> res = new LinkedList<Wrapper<? extends PDefinition>>();
+		    AFunctionParagraphDefinition fd = (AFunctionParagraphDefinition) w.value;
+		    res = DefinitionMap.getDelegate(fd.getClass())
+			    .extractSubdefinition(fd);
+		    return res.toArray();
+		}
+	    }
+	    return new String[0];
+	} catch (Exception e) {
+	    return new String[] { SMILING_ERROR_STRING };
 	}
+    }
+
+    private List<Wrapper<? extends PDefinition>> handleChansetParagraphDefinition(
+	    AChansetParagraphDefinition cspdef) {
+	List<Wrapper<? extends PDefinition>> r = new LinkedList<Wrapper<? extends PDefinition>>();
+	for (AChansetDefinition cdef : cspdef.getChansets()) {
+	    r.add(Wrapper.newInstance(cdef, cdef.getIdentifier().toString()));
+	}
+	return r;
+    }
+
+    private List<Wrapper<? extends PDefinition>> handleClassParagraphDefinition(
+	    AClassParagraphDefinition cpdef) {
+	List<Wrapper<? extends PDefinition>> r = new LinkedList<Wrapper<? extends PDefinition>>();
+	for (PDefinition pdef : cpdef.getDefinitions()) {
+	    r.addAll(DefinitionMap.getDelegate(pdef.getClass())
+		    .extractSubdefinition(pdef));
+	}
+	return r;
+    }
+
+    private List<Wrapper<? extends PDefinition>> handleValueParagraphDefinition(
+	    AValueParagraphDefinition cast) {
+	return DefinitionMap.getDelegate(cast.getClass()).extractSubdefinition(
+		cast);
+    }
+
+    @Override
+    public Object getParent(Object element) {
+	if (element instanceof Wrapper) {
+	    Wrapper w = (Wrapper) element;
+	    if (w.value instanceof INode) {
+		INode in = (INode) w.value;
+		return Wrapper.newInstance(in.parent(), in.parent().toString());
+		// return ((INode) w.value).parent();}
+	    }
+	}
+	return null;
+    }
+
+    // TODO - correct hasChildren method for reference processes at the highest
+    // level
+    @Override
+    public boolean hasChildren(Object element) {
+	if (element instanceof Wrapper) {
+	    Wrapper w = (Wrapper) element;
+	    if (w.value instanceof AReferenceProcess)
+		return false;
+	    return true;
+	}
+
+	return false;
+    }
+
+    private List<Wrapper<? extends INode>> handleReferenceProcess(PProcess pp) {
+	if (ProcessMap.getDelegate(pp.getClass()) != null)
+	    return ProcessMap.getDelegate(pp.getClass()).makeEntries(pp);
+	return new LinkedList<Wrapper<? extends INode>>();
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Wrapper<? extends INode>> handleProcessParagraphDefinition(
+	    AProcessParagraphDefinition ppdef) {
+	PProcess pp = ppdef.getProcessDefinition().getProcess();
+	if (ProcessMap.getDelegate(pp.getClass()) != null)
+	    return ProcessMap.getDelegate(pp.getClass()).makeEntries(pp);
+	return new LinkedList<Wrapper<? extends INode>>();
+    }
+
+    private List<Wrapper<AChannelNameDefinition>> handleChannelParagraphDefinition(
+	    AChannelParagraphDefinition cpdef) {
+	List<Wrapper<AChannelNameDefinition>> r = new LinkedList<Wrapper<AChannelNameDefinition>>();
+	for (AChannelNameDefinition dec : cpdef.getChannelNameDeclarations()) {
+	    r.add(Wrapper.newInstance(dec, dec.getSingleType().getIdentifiers()
+		    .toString()
+		    + ": " + dec.getSingleType().getType()));
+	}
+	return r;
+    }
 }
