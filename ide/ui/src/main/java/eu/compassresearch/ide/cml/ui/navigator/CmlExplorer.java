0dad7e6351173eeb70f52a42ac6de514f3bf16c1
Luis Diogo Couto <lcouto@iha.dk>
2013-04-14 18:59:07 +0200

    Cml perspective now loading by default.

---
+++ ide/ui/src/main/java/eu/compassresearch/ide/cml/ui/navigator/CmlExplorer.java
@@ -0,0 +1,14 @@
+package eu.compassresearch.ide.cml.ui.navigator;
+
+import org.eclipse.ui.navigator.CommonNavigator;
+
+public class CmlExplorer extends CommonNavigator 
+{
+
+	public CmlExplorer() {
+		super();
+		super.setLinkingEnabled(true);			
+	}
+	
+	
+}
