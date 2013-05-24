a70f09f93129aaae2ca25a452a9ecc1a2a95242e
simondf <simon.foster@york.ac.uk>
2013-05-24 17:26:16 +0200

    Added a basic visitor for mapping PTypes to Isabelle and some associated tests

--- core/analysis/theoremprover/src/test/java/eu/compassresearch/theoremprover/TPVisitorTest1.java
+++ core/analysis/theoremprover/src/test/java/eu/compassresearch/theoremprover/TPVisitorTest1.java
@@ -3,6 +3,7 @@ package eu.compassresearch.theoremprover;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,27 +14,30 @@ import eu.compassresearch.ast.program.PSource;
 
 public class TPVisitorTest1 {
 	
-	public static String simpleTypePath  = "src/test/resources/SimpleType.cml";
+	public static String compoundTypesPath  = "src/test/resources/CompoundsTypes.cml";
+	public static String basicTypesPath  = "src/test/resources/SimpleType.cml";
 	
 	@Test
-	public void testSimpleType() throws AnalysisException, IOException{
-		test(simpleTypePath);
+	public void testBasicTypes() throws AnalysisException, IOException{
+		List<String> expRes = new LinkedList<String>();
+		Collections.addAll(expRes, "@nat", "@int", "@char");
+		test(basicTypesPath, expRes);	
+	}
+	
+	@Test
+	public void testCompoundsTypes() throws AnalysisException, IOException{
+		List<String> expRes = new LinkedList<String>();
+		Collections.addAll(expRes, "@seq of @nat", "@set of @real", "@seq1 of @char");
+		test(compoundTypesPath, expRes);	
 	}
 	
 	
-	
-	
 
-	public void test(String filePath) throws AnalysisException, IOException {
+	public void test(String filePath, List<String> expRes) throws AnalysisException, IOException {
 		TPVisitor tpVisitor = new TPVisitor();
 		PSource ast = TPUtil.makeSourceFromFile(filePath);
 		
 		ast.apply(tpVisitor);
-		
-		List<String> expRes = new LinkedList<String>();		
-		expRes.add("basic");
-		expRes.add("basic2");
-		
 		List<String> actRes = tpVisitor.getTypeList();
 		
 		assertEquals(expRes, actRes);
