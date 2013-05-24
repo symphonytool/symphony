aaeaa8d1b4d15e0dc9731094c72e89541278cf03
simondf <simon.foster@york.ac.uk>
2013-05-24 15:34:12 +0200

    Added the required dependencies for Isabelle so that the COMPASS tool build pulls them in

---
+++ ide/tp-plugin/src/main/java/eu/compassresearch/ide/cml/tpplugin/GenerateTheory.java
@@ -0,0 +1,58 @@
+package eu.compassresearch.ide.cml.tpplugin;
+import isabelle.Isabelle_Process;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.overture.ast.analysis.AnalysisException;
+import org.overture.ide.core.resources.VdmSourceUnit;
+
+import eu.compassresearch.ide.cml.ui.editor.core.dom.CmlSourceUnit;
+import eu.compassresearch.theoremprover.TPVisitor;
+
+public class GenerateTheory {
+
+	public static void toThy(IFile cmlFile) throws IOException {
+		
+		String cmlLoc = cmlFile.getLocation().toString(); 
+		String thyFile = cmlLoc.replaceAll("\\.cml", "\\.thy");
+		
+		File tempPo = new File(thyFile);
+
+		Isabelle_Process ip;
+		
+		FileWriter fw;
+		
+		fw = new FileWriter(tempPo);
+		
+		CmlSourceUnit source = (CmlSourceUnit) cmlFile.getAdapter(VdmSourceUnit.class);
+		
+		TPVisitor tpVisitor = new TPVisitor();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			source.getSourceAst().apply(tpVisitor);
+		} catch (AnalysisException e) {
+			// TODO Auto-generated catch block
+			sb.append("Analysis Error");
+			e.printStackTrace();
+		}
+		
+		for (String s:tpVisitor.getTypeList()) {
+			sb.append(s);
+			sb.append("\n\n");
+		}
+		
+		fw.write(sb.toString());
+		fw.flush();
+		fw.close();
+
+		// File fileToOpen = new File(workspaceLoc, thyFile);
+
+	}
+	
+}
