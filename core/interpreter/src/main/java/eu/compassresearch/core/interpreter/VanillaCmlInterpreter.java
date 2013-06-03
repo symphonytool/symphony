005df79e03a32ae2fce0937b5af452823925f383
anderskm <akm@iha.dk>
2013-06-03 11:57:37 +0200

    Replicated inteleaving test is fixed

--- core/interpreter/src/main/java/eu/compassresearch/core/interpreter/VanillaCmlInterpreter.java
+++ core/interpreter/src/main/java/eu/compassresearch/core/interpreter/VanillaCmlInterpreter.java
@@ -313,7 +313,7 @@ class VanillaCmlInterpreter extends AbstractCmlInterpreter
 	public static void main(String[] args) throws IOException, InterpreterException
 	{
 		File cml_example = new File(
-				"src/test/resources/action/replicated/replicated-interleaving.cml");
+				"src/test/resources/action/replicated/replicated-interleaving2.cml");
 		runOnFile(cml_example);
 
 	}
