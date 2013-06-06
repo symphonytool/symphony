9245d9cc552dfa7d76d96b999bb9194c4f667900
simondf <simon.foster@york.ac.uk>
2013-06-06 15:51:28 +0100

    Fixed up IsabelleTheory so that proof completion etc. can be properly detected and added some sanity tests which start and stop Isabelle and try some simple proofs out.

--- core/analysis/theoremprover/src/main/java/eu/compassresearch/theoremprover/IsabelleTheory.scala
+++ core/analysis/theoremprover/src/main/java/eu/compassresearch/theoremprover/IsabelleTheory.scala
@@ -3,6 +3,7 @@ package eu.compassresearch.theoremprover
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
 import isabelle.{Build, Document, Session, Text, Protocol, Thy_Load, Command}
+import isabelle.Document.Node
 import eu.compassresearch.core.common.AnalysisArtifact
 import java.io.File
 import java.io.FileWriter
@@ -10,7 +11,9 @@ import java.io.FileWriter
 object IsabelleTheory {
   
 
-  val isabelleHome = "/local/d0p6/simonf/Isabelle/Isabelle2013"
+  val isabelleHome = if (System.getenv("ISABELLE_HOME") == null) 
+	  					"/local/d0p6/simonf/Isabelle/Isabelle2013" 
+                     else System.getenv("ISABELLE_HOME")
   
   def createHOLSession(): Session = {
     System.setProperty("isabelle.home", isabelleHome)
@@ -26,24 +29,31 @@ class IsabelleTheory ( val session: Session
                      , val thyName: String
                      , val thyDir: String) extends AnalysisArtifact {
 
-  sealed abstract class IsabelleProof
+  sealed abstract class IsabelleProof {
+    var location: Option[Text.Offset]= None
+    def qedOffset(): Text.Offset
+  }
   case class IsabelleByProof(val proof: String) extends IsabelleProof {
-    var command: Option[Command]= None
-    override def toString() = "by ("+ proof +")" + ls 
+    override def toString() = "by ("+ proof +")" + ls
+    override def qedOffset() = 0
   }
 
-  case class IsabelleApplyProof(val proof: List[String]) {
+  case class IsabelleApplyProof(val proof: List[String]) extends IsabelleProof {
+    val applyString = proof.map("apply (" + _ + ")" + ls).mkString("") 
     override def toString() = {
-      proof.map("  apply (" + _ + ")" + ls).mkString("") + "done" + ls
+       applyString + "done" + ls
     }
+    override def qedOffset() = applyString.length()
   }
 
   
   class IsabelleTheorem( val name: String
                        , val goal: String
                        , var proof: IsabelleProof) {
+    var location: Option[Text.Offset] = None
+    def qedLocation() = proof.location.map(_ + proof.qedOffset())
     val lemmaString: String = "lemma " + name +": \"" + goal + "\"" + ls 
-	val proofString: String = proof.toString();
+	def proofString: String = proof.toString();
     def thmString: String = lemmaString + proofString
   
     def this(name: String, goal:String, proof: String) {
@@ -52,7 +62,56 @@ class IsabelleTheory ( val session: Session
   
     def this(name: String, goal:String) {
       this(name, goal, "oops")
-    }  
+    }
+    
+    def updateLocation(l: Text.Offset) = {
+      location = Some(l)
+      updateProofLocation()
+    }
+    
+    def writeHead() {
+      location match {
+        case Some(l) => {
+          session.update(List(thyNode -> Node.Edits(List(Text.Edit.insert(l,lemmaString)))))
+        }
+        case None => return
+      }
+    }
+    
+    def writeProof() {
+      location match {
+        case Some(l) => {
+          session.update(List(thyNode -> Node.Edits(List(Text.Edit.insert(l + lemmaString.length(),proofString)))))
+        }
+        case None => return
+      }
+    }
+    
+    def deleteProof() {
+      location match {
+        case Some(l) => {
+          session.update(List(thyNode -> Node.Edits(List(Text.Edit.remove(l + lemmaString.length(),proofString)))))
+        }
+        case None => return
+      }
+    }
+   
+    def updateProofLocation() {
+      location match {
+        case Some(l) => {
+          proof.location = Some(l + lemmaString.length())
+        }
+        case None => return
+      }
+    }
+    
+    def updateProof(p: IsabelleProof) {
+      deleteProof()
+      proof = p
+      writeProof()
+      updateProofLocation()
+    }
+    
   }
 
   private val ls = System.getProperty("line.separator")
@@ -61,11 +120,11 @@ class IsabelleTheory ( val session: Session
   val thyHead = "theory " + thyName + ls + "imports Main" + ls + "begin" + ls + ls
   val thyTail  = ls + "end" + ls
   val header = session.thy_load.check_thy_text(thyNode, thyHead)
-  private var thms: List[IsabelleTheorem] = Nil
+  var thms: List[IsabelleTheorem] = Nil
   
-  private def thyBody = thms.map(_.thmString).mkString(ls)
-  private def thmEnd = thyHead.length() + thyBody.length(); 
-  private def thyEnd = thmEnd + 10;
+  def thyBody = thms.map(_.thmString).mkString(ls)
+  def thmEnd = thyHead.length() + thyBody.length(); 
+  def thyEnd = thmEnd + 10;
   
   
   def init() {
@@ -82,13 +141,37 @@ class IsabelleTheory ( val session: Session
     );
   }
   
-  def addThm(thm: IsabelleTheorem) {
-    val oldThmEnd = thmEnd;
-    thms = thms ++ List(thm);
-    val newThmEnd = thmEnd;
+  def updatePerspective() {
     val perspective = Text.Perspective(List(Text.Range(0, thyEnd)))
-    session.update(List( thyNode -> Document.Node.Edits(List(Text.Edit.insert(oldThmEnd,thm.thmString)))
-                       , thyNode -> Document.Node.Perspective(perspective)));
+    session.update(List(thyNode -> Document.Node.Perspective(perspective)));
+  }
+  
+  def addThm(thm: IsabelleTheorem) {
+    val oldThmEnd = thmEnd
+    thm.updateLocation(oldThmEnd)
+    thms = thms ++ List(thm)
+    val newThmEnd = thmEnd
+    val perspective = Text.Perspective(List(Text.Range(0, thyEnd)))
+    thm.writeHead()
+    thm.writeProof()
+    updatePerspective()
+  }
+
+  def updateProof(thmName: String, proof: IsabelleProof) {
+    val thmList = thms.dropWhile(_.name != thmName)
+    thmList match {
+      case (t :: ts) => {
+        val op = t.proofString
+        t.updateProof(proof)
+        val np = t.proofString
+        updatePerspective()
+        // Every theorem following must have its location updated
+        for (t1 <- ts) {
+          t1.location.map(_ + (np.length() - op.length())) 
+        }
+      }
+      case Nil => return
+    }
   }
   
   override def toString() = thyHead + thyBody + thyTail
@@ -104,19 +187,24 @@ class IsabelleTheory ( val session: Session
   }
   
   
-  /**
-   * Checks if the contents of this document have already been submitted as-is to the prover
-   */
-  def thmCmd(thm : String): Option[Command] = {
-    val s = session.snapshot(thyNode)
-    val cmds = s.node.commands.toList  
-    cmds.find(c => c.source.contains(thm))
+  // Gets the theorem command by looking at the appropriate location in the current snapshot
+  def thmCmd(thmName : String): Option[Command] = {
+    val thm  = thms.find(_.name == thmName)
+    val node = session.snapshot(thyNode).node
+    thm.flatMap(_.location.flatMap(node.command_at(_))).map(_._1)
+  }
+
+  def qedCmd(thmName : String): Option[Command] = {
+    val thm  = thms.find(_.name == thmName)
+    val node = session.snapshot(thyNode).node
+    thm.flatMap(_.qedLocation().flatMap(node.command_at(_))).map(_._1)
   }
   
+/*
   def proofCmds(thm : String): Option[List[Command]] = {
     val s = session.snapshot(thyNode)
     val cmds = s.node.commands.toList
-    var found = false;
+    var found = false
     var prf = false;
     var prfcmds : List[Command] = Nil
     for (c <- cmds) {
@@ -140,7 +228,9 @@ class IsabelleTheory ( val session: Session
     
     if (found) { Some(prfcmds) } else { None }
   }
+*/
 
+/*
   def thmStatus(thm : String): Option[Protocol.Status] = {
     val cmd = proofCmds(thm).flatMap(_.lastOption)
     val version = session.snapshot().version
@@ -153,9 +243,23 @@ class IsabelleTheory ( val session: Session
       case None => None
     }
   }
- 
+*/
+  
+  def thmStatus(thm : String): Option[Protocol.Status] = {
+    val cmd = qedCmd(thm)
+    val version = session.snapshot().version
+    val state   = session.snapshot().state
+    cmd match {
+      case Some(c) => {
+    	val st = state.command_state(version, c)
+    	Some(Protocol.command_status(st.status))
+      }
+      case None => None
+    }
+  }
+  
   def thmIsProved(thm : String): Boolean = {
-    val cmd = proofCmds(thm).flatMap(_.lastOption)
+    val cmd = qedCmd(thm)
     val status = thmStatus(thm)
     (status.exists(x => x.is_finished && !x.is_failed) 
       && cmd.exists(x => !(x.source.contains("oops") || x.source.contains("sorry"))))
--- core/analysis/theoremprover/src/test/java/eu/compassresearch/theoremprover/IsabelleTheoryTests.java
+++ core/analysis/theoremprover/src/test/java/eu/compassresearch/theoremprover/IsabelleTheoryTests.java
@@ -1,6 +1,8 @@
 package eu.compassresearch.theoremprover;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import isabelle.Outer_Syntax;
 import isabelle.Session;
@@ -15,33 +17,83 @@ import org.overture.ast.analysis.AnalysisException;
 
 public class IsabelleTheoryTests {
 
+	public static int isabelleTimeout = 30000;
+	public static int proofTimeout = 5000;
+    public static int waitInterval = 200;
+	
 	public static String emptyThyPath  = "src/test/resources/Empty.thy";
 	public static String twoLemmasThyPath  = "src/test/resources/TwoLemmas.thy";
 
+	public Session startIsabelleHOL() throws InterruptedException {
+		System.out.print("Isabelle/HOL loading...");
+		Session s = IsabelleTheory.createHOLSession();
+		int t = isabelleTimeout;
+		while (!s.is_ready() && t > 0) {
+			System.out.print(".");
+			Thread.sleep(waitInterval);
+			t -= waitInterval;
+		}
+		System.out.println("done");
+		return s;
+	}
+	
 	@Test
 	public void testEmptyThy() throws AnalysisException, IOException{
-		Session sess = new Session(new Thy_Load(null, Outer_Syntax.empty()));
+		Session s = IsabelleTheory.createHOLSession();
 		
-		IsabelleTheory ithy = new IsabelleTheory(sess, "Empty", "src/test/resources");
+		IsabelleTheory ithy = new IsabelleTheory(s, "Empty", "src/test/resources");
 		
 		test(emptyThyPath, ithy);	
 	}
 
 	@Test
-	public void testTwoLemmas() throws AnalysisException, IOException {		
-		Session s = TPUtilScala.createHOLSession();
+	public void startStopIsabelle() throws AnalysisException, IOException, InterruptedException{
+		Session s = startIsabelleHOL();
+		s.stop();
+	}
+
+	@Test
+	public void createIsabelleTheory() throws AnalysisException, IOException, InterruptedException{
+		Session s = startIsabelleHOL();
+		IsabelleTheory ithy = new IsabelleTheory(s, "Empty", "src/test/resources");
+		ithy.init();
+		s.stop();
+	}
+
+	
+	
+	
+	@Test
+	public void testTwoLemmas() throws AnalysisException, IOException, InterruptedException {		
+		Session s = startIsabelleHOL();
 		
 		IsabelleTheory ithy = new IsabelleTheory(s, "TwoLemmas", "src/test/resources");
 		ithy.init();
 		ithy.addThm(ithy.new IsabelleTheorem("Lemma1", "True", "simp"));
-		ithy.addThm(ithy.new IsabelleTheorem("Lemma2", "True", "simp"));
+		ithy.addThm(ithy.new IsabelleTheorem("Lemma2", "False", "simp"));
 		
-		test(twoLemmasThyPath, ithy);
+		int t = proofTimeout;
 		
+		System.out.print("Waiting for proof to complete...");
+		while (!ithy.thmIsProved("Lemma1") && t > 0)
+		{			
+			System.out.print(".");
+			Thread.sleep(waitInterval);
+			t -= waitInterval;
+		}
+		
+		System.out.println("done");
+		
+		boolean l1 = ithy.thmIsProved("Lemma1");		
+		boolean l2 = ithy.thmIsProved("Lemma2");
+		
+		assertTrue(l1);
+		assertFalse(l2);
+
 		s.stop();
 		
 	}
-	
+
 	public void test(String filePath, IsabelleTheory ithy)
 			throws AnalysisException, IOException {
     	BufferedReader br = new BufferedReader(new FileReader(filePath));
