785887a3b49d0a84c6952aaceb5ff7cf6f17410c
anderskm <akm@iha.dk>
2013-07-29 11:28:45 +0200

    The typechecker are now runs successfully on all the tests

--- core/typechecker/src/main/visitors/eu/compassresearch/core/typechecker/TCActionVisitor.java
+++ core/typechecker/src/main/visitors/eu/compassresearch/core/typechecker/TCActionVisitor.java
@@ -484,40 +484,39 @@ class TCActionVisitor extends
 		
 		//Since we are in a empty scope we must add the process section except for the state
 		//since these will be added given the namesetexp
-		AActionProcess actionProcessNode = node.getAncestor(AActionProcess.class);
-		if(actionProcessNode != null)
-		{
-			for(PDefinition pdef : actionProcessNode.getDefinitionParagraphs())
-			{
-				//TODO this should be removed once the operations definition are unwrapped
-				if(pdef instanceof AOperationsDefinition)
-				{
-					for(PDefinition op : ((AOperationsDefinition) pdef).getOperations())
-					{
-						actionEnv.addVariable(op.getName(),op);
-					}
-				}
-				else if(pdef instanceof AActionsDefinition)
-				{
-					for(PDefinition action : ((AActionsDefinition) pdef).getActions())
-					{
-						actionEnv.addVariable(action.getName(),action);
-					}
-				}
-				else if(pdef instanceof AFunctionsDefinition)
-				{
-					for(PDefinition func : ((AFunctionsDefinition) pdef).getFunctionDefinitions())
-					{
-						actionEnv.addVariable(func.getName(),func);
-					}
-				}
-				//else if(pdef instanceof AAssignmentDefinition)
-				//	continue;
-				else
-					actionEnv.addVariable(pdef.getName(), pdef);
-			}
-		}
-		
+//		AActionProcess actionProcessNode = node.getAncestor(AActionProcess.class);
+//		if(actionProcessNode != null)
+//		{
+//			for(PDefinition pdef : actionProcessNode.getDefinitionParagraphs())
+//			{
+//				//TODO this should be removed once the operations definition are unwrapped
+//				if(pdef instanceof AOperationsDefinition)
+//				{
+//					for(PDefinition op : ((AOperationsDefinition) pdef).getOperations())
+//					{
+//						actionEnv.addVariable(op.getName(),op);
+//					}
+//				}
+//				else if(pdef instanceof AActionsDefinition)
+//				{
+//					for(PDefinition action : ((AActionsDefinition) pdef).getActions())
+//					{
+//						actionEnv.addVariable(action.getName(),action);
+//					}
+//				}
+//				else if(pdef instanceof AFunctionsDefinition)
+//				{
+//					for(PDefinition func : ((AFunctionsDefinition) pdef).getFunctionDefinitions())
+//					{
+//						actionEnv.addVariable(func.getName(),func);
+//					}
+//				}
+//				//else if(pdef instanceof AAssignmentDefinition)
+//				//	continue;
+//				else
+//					actionEnv.addVariable(pdef.getName(), pdef);
+//			}
+//		}
 		
 		//CmlTypeCheckInfo actionEnv = cmlEnv.newScope();
 		actionEnv.scope = NameScope.NAMESANDANYSTATE;
@@ -560,7 +559,7 @@ class TCActionVisitor extends
 		}
 
 		PVarsetExpression sexp = node.getNamesetExpression();
-		PType sexpType = sexp.apply(parentChecker, actionEnv);
+		PType sexpType = sexp.apply(parentChecker, question);
 		if (!successfulType(sexpType)) {
 			node.setType(issueHandler.addTypeError(node,
 					TypeErrorMessages.COULD_NOT_DETERMINE_TYPE
