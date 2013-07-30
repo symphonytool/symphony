6982995e515e4a1ff82d451d804262d517d9fa9a
Kenneth Lausdahl <kel@iha.dk>
2013-07-30 15:31:51 +0200

    fixed parser error for
    only EOF
    
    process folowed by commant but no \n
    
    added lexer error collection and reporting in the IDE - this was completly missing and probably lead to some siturations where a model looked ok but had lex errors

---
+++ core/parser/src/main/java/eu/compassresearch/core/parser/CmlParserError.java
@@ -0,0 +1,33 @@
+package eu.compassresearch.core.parser;
+
+import java.io.File;
+
+import org.antlr.runtime.RecognitionException;
+import org.overture.ast.intf.lex.ILexLocation;
+import org.overture.ast.lex.LexLocation;
+
+public class CmlParserError
+{
+	public final String message;
+	public final RecognitionException error;
+	
+	final int line;
+	final int charPositionInLine;
+	final int startIndex;
+	final int stopIndex;
+
+	public CmlParserError(String message, RecognitionException e, int line, int charPositionInLine, int startIndex, int stopIndex)
+	{
+		this.message = message;
+		this.error = e;
+		this.line = line;
+		this.charPositionInLine = charPositionInLine;
+		this.startIndex = startIndex;
+		this.stopIndex=stopIndex;
+	}
+	
+	public ILexLocation getLocation(File sourceFile)
+	{
+		 return new LexLocation(sourceFile, "", line, charPositionInLine, line, charPositionInLine, startIndex, stopIndex);
+	}
+}
