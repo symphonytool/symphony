f588a1a9d8aee0130e465f65d8aa4acfd5082d2d
Joey Coleman <jwc@iha.dk>
2013-08-01 15:20:40 +0200

    We need these POG files as they are the new ones.
    This reverts commit ffda24d6416b2378b6b6665a51cd80d4bc640c3c.

---
+++ core/analysis/pog/src/main/java/eu/compassresearch/core/analysis/pog/obligations/CmlProofObligationList.java
@@ -0,0 +1,36 @@
+/******************
+ *
+ * Inspired by Proof Obligation List from Overture
+ * Class as complex list Datatype, stores collection 
+ * of Proof Obligation objects
+ *
+ *******************/
+
+package eu.compassresearch.core.analysis.pog.obligations;
+
+import java.util.Vector;
+
+import org.overture.pog.pub.IProofObligation;
+import org.overture.pog.pub.IProofObligationList;
+
+import eu.compassresearch.core.common.AnalysisArtifact;
+
+@SuppressWarnings("serial")
+public class CmlProofObligationList extends Vector<IProofObligation> implements
+		AnalysisArtifact, IProofObligationList {
+
+	@Override
+	public void trivialCheck() {
+		// only used in overture
+
+	}
+
+	@Override
+	public void renumber() {
+		int n = 0;
+		for (IProofObligation po : this) {
+			po.setNumber(n++);
+		}
+	}
+
+}
