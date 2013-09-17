8b23f21a945ddc664a422e4dc84a67207a8f9383
Claus Nielsen <clausbn@iha.dk>
2013-09-17 19:16:40 +0200

    Added initial collaboration plugin project

---
+++ ide/plugins/collaboration/src/main/java/eu/compassresearch/ide/collaboration/Activator.java
@@ -0,0 +1,24 @@
+package eu.compassresearch.ide.collaboration;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator
+{
+
+	@Override
+	public void start(BundleContext arg0) throws Exception
+	{
+
+	}
+
+	@Override
+	public void stop(BundleContext arg0) throws Exception
+	{
+		/*
+		 * Do what needs to be done for tearing down this plug-in
+		 */
+
+	}
+
+}
