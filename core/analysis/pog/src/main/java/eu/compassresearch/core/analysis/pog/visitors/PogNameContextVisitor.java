7d8df529c052732eb6d90cb331b8363af4c2a71b
Luis Diogo Couto <lcouto@iha.dk>
2013-10-16 16:01:39 +0200

    Handling the null name of process state definitions.

--- core/analysis/pog/src/main/java/eu/compassresearch/core/analysis/pog/visitors/PogNameContextVisitor.java
+++ core/analysis/pog/src/main/java/eu/compassresearch/core/analysis/pog/visitors/PogNameContextVisitor.java
@@ -1,10 +1,15 @@
 package eu.compassresearch.core.analysis.pog.visitors;
 
 import org.overture.ast.analysis.AnalysisException;
+import org.overture.ast.analysis.intf.IAnswer;
+import org.overture.ast.definitions.AClassInvariantDefinition;
+import org.overture.ast.definitions.AStateDefinition;
 import org.overture.ast.definitions.PDefinition;
 import org.overture.ast.lex.LexNameList;
 import org.overture.ast.node.INode;
 import org.overture.pog.obligation.PONameContext;
+import org.overture.pog.pub.IPogAssistantFactory;
+import org.overture.pog.utility.POException;
 import org.overture.typechecker.assistant.definition.PDefinitionAssistantTC;
 
 import eu.compassresearch.ast.analysis.AnswerCMLAdaptor;
@@ -13,6 +18,7 @@ import eu.compassresearch.ast.definitions.AExplicitCmlOperationDefinition;
 import eu.compassresearch.ast.definitions.AImplicitCmlOperationDefinition;
 import eu.compassresearch.ast.definitions.AProcessDefinition;
 import eu.compassresearch.ast.definitions.PCMLDefinition;
+import eu.compassresearch.ast.lex.LexNameToken;
 
 public class PogNameContextVisitor extends AnswerCMLAdaptor<PONameContext>
 {
@@ -24,20 +30,55 @@ public class PogNameContextVisitor extends AnswerCMLAdaptor<PONameContext>
 
 	//FIXME Add/Incorporate Overture Assistants
 	
+	IPogAssistantFactory af;
+	
+	
+	
+	
+	public PogNameContextVisitor()
+	{
+		af=new CmlPogAssistantFactory();
+	}
+
 	@Override
 	public PONameContext defaultPDefinition(PDefinition node) throws AnalysisException
 	{
-		return new PONameContext(PDefinitionAssistantTC.getVariableNames(node));
+		return new PONameContext(node.apply(af.getVariableNameCollector()));
 	}
 	
-	//FIXME Do we need a name handler for friggin' processes?
-
+	//Need to code around process state since it has no name
+	
+	@Override
+	public PONameContext caseAStateDefinition(AStateDefinition node)
+			throws AnalysisException
+	{
+		LexNameList variableNames = new LexNameList();
+		
+		for (PDefinition d : node.getStateDefs())
+		{
+			if (d instanceof AClassInvariantDefinition){
+				AProcessDefinition apd= node.getAncestor(AProcessDefinition.class);
+				if (apd==null)
+					throw new	POException(d, "Could not locate respective process definition");
+				variableNames.add(new LexNameToken("", apd.getName().getName()+"State", d.getLocation().clone()));		
+			}
+			else 
+			{
+				
+				variableNames.addAll(d.apply(af.getVariableNameCollector()));
+			}
+		}
+		return new PONameContext(variableNames);
+	}
+	
 	@Override
 	public PONameContext caseAActionDefinition(AActionDefinition node)
 			throws AnalysisException
 	{
 		return new PONameContext(new LexNameList(node.getName()));
 	}
+
+
 
 	@Override
 	public PONameContext caseAProcessDefinition(AProcessDefinition node)
