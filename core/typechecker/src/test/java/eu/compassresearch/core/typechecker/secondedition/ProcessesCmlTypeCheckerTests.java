1a113b7f054f416f320f2cec6bd2fa672f198991
Kenneth Lausdahl <kel@iha.dk>
2013-10-28 14:38:02 +0100

    enabled more csp tests

---
+++ core/typechecker/src/test/java/eu/compassresearch/core/typechecker/secondedition/ProcessesCmlTypeCheckerTests.java
@@ -0,0 +1,31 @@
+package eu.compassresearch.core.typechecker.secondedition;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(value = Parameterized.class)
+public class ProcessesCmlTypeCheckerTests extends
+		AbstractResultBasedCmlTypeCheckerTestCase
+{
+	public ProcessesCmlTypeCheckerTests(File file, String name, TestType type)
+	{
+		super(file, name, type);
+	}
+
+	@Parameters(name = "{1}")
+	public static Collection<Object[]> getData()
+	{
+		return combine(collectResourcesTestData("processes", TestType.POSITIVE),collectResourcesTestData("processes", TestType.NEGATIVE));
+	}
+
+	// @Before
+	// public void setup()
+	// {
+	// Properties.recordTestResults = true;
+	// }
+
+}
