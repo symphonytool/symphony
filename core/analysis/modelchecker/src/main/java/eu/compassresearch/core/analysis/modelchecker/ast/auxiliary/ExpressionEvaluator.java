ce272fd483b65d7e6e679b76d46a3bcc37841be8
Alysson Filgueira <alyssonfilgueira@gmail.com>
2013-11-08 16:34:16 +0100

    More changes to handle minimondex.

--- core/analysis/modelchecker/src/main/java/eu/compassresearch/core/analysis/modelchecker/ast/auxiliary/ExpressionEvaluator.java
+++ core/analysis/modelchecker/src/main/java/eu/compassresearch/core/analysis/modelchecker/ast/auxiliary/ExpressionEvaluator.java
@@ -328,6 +328,8 @@ public class ExpressionEvaluator implements IExpressionEvaluator {
 		
 		if(type instanceof MCAIntNumericBasicType){
 			result = new MCAIntLiteralExp("0");
+		} else if(type instanceof MCANatNumericBasicType){
+			result = new MCAIntLiteralExp("0");
 		}
 		
 		return result;
