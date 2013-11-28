baeda172a181f91ce95cf10de36a2d6a47a52f84
Kenneth Lausdahl <kel@iha.dk>
2013-11-28 14:57:07 +0100

    enabled external code calls from cml
    this requires sync with overture: b1231553295009d2c3313aade51e259807205ace

---
+++ core/interpreter/src/test/java/eu/compassresearch/core/interpreter/testing/classes/external/A.java
@@ -0,0 +1,18 @@
+package eu.compassresearch.core.interpreter.testing.classes.external;
+import org.overture.interpreter.runtime.ValueException;
+import org.overture.interpreter.values.IntegerValue;
+import org.overture.interpreter.values.Value;
+
+public class A
+{
+	public static Value getHello()
+	{
+		System.err.println("Yep I got in here");
+		return new IntegerValue(2);
+	}
+	
+	public static Value add(Value a, Value b) throws ValueException
+	{
+		return new IntegerValue(a.intValue(null) + b.intValue(null));
+	}
+}
