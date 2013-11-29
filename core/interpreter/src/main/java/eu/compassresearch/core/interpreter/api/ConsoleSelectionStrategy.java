bc7f1934fa78337e3235f264f54a311eb6b0e5e5
Kenneth Lausdahl <kel@iha.dk>
2013-11-29 01:37:11 +0100

    suppressed warnings

--- core/interpreter/src/main/java/eu/compassresearch/core/interpreter/api/ConsoleSelectionStrategy.java
+++ core/interpreter/src/main/java/eu/compassresearch/core/interpreter/api/ConsoleSelectionStrategy.java
@@ -45,6 +45,7 @@ public class ConsoleSelectionStrategy implements SelectionStrategy
 		return silentTransitions.size() > count;
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private CmlTransition systemSelect(CmlTransitionSet availableChannelEvents)
 	{
 		rndSelect.choices(new CmlTransitionSet((Set) availableChannelEvents.getSilentTransitionsAsSet()));
