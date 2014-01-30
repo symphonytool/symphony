e8669a36abc85d1798d1f0ad4cc0dcf2dca62fb0
Luis Diogo Couto <ldc@eng.au.dk>
2014-01-30 14:28:33 +0100

    Restored version 1 of PTP.

--- ide/plugins/theoremprover/src/main/java/eu/compassresearch/ide/theoremprover/TPConstants.java
+++ ide/plugins/theoremprover/src/main/java/eu/compassresearch/ide/theoremprover/TPConstants.java
@@ -5,9 +5,8 @@ public interface TPConstants
 public static final String PLUGIN_ID = "eu.compassresearch.ide.theoremprover";
 public static final String TP_PERSPECTIVE_ID = "isabelle.eclipse.ui.theoryPerspective";
 public static final String NEEDS_PROJECT_PARAM_ID = "eu.compassresearch.ide.theoremprover.commands.needproj";
-
 public static final String PROOF_SESSION_ID = "eu.compassresearch.ide.theoremprover.proofsess";
-
+public static final String ISABELLE_PERSPECTIVE_ID = "isabelle.eclipse.ui.theoryPerspective";
 public static final int BY_CML_AUTO_TAC_OFFSET = 17;
 
 }
