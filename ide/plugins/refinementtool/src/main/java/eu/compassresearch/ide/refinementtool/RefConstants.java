0cbfdd1734f533f566c85c7667ab7990313b2e58
Simon Foster <simon.foster@york.ac.uk>
2014-03-31 14:59:54 +0100

    Added a basic perspective for the refinement tool

---
+++ ide/plugins/refinementtool/src/main/java/eu/compassresearch/ide/refinementtool/RefConstants.java
@@ -0,0 +1,8 @@
+package eu.compassresearch.ide.refinementtool;
+
+public abstract class RefConstants
+{
+	public static final String REF_REGISTRY_ID = "REF_REGISTRY";
+	
+	public final static String PLUGIN_ID = "eu.compassresearch.ide.refinementtool";
+}
