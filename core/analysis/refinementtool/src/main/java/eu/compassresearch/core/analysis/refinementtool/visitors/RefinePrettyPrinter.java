a3bad148cf7f6ca71c5b4cf9d2f2bb564df93557
Simon Foster <simon.foster@york.ac.uk>
2014-04-10 15:56:52 +0100

    Added a couple of methods

--- core/analysis/refinementtool/src/main/java/eu/compassresearch/core/analysis/refinementtool/visitors/RefinePrettyPrinter.java
+++ core/analysis/refinementtool/src/main/java/eu/compassresearch/core/analysis/refinementtool/visitors/RefinePrettyPrinter.java
@@ -2,9 +2,13 @@ package eu.compassresearch.core.analysis.refinementtool.visitors;
 
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.node.INode;
+import org.overture.ast.statements.AAssignmentStm;
 
 import eu.compassresearch.ast.analysis.QuestionAnswerCMLAdaptor;
+import eu.compassresearch.ast.definitions.AActionDefinition;
 import eu.compassresearch.ast.expressions.ABracketedExp;
+import eu.compassresearch.ast.process.AActionProcess;
+import eu.compassresearch.ast.statements.AActionStm;
 
 public class RefinePrettyPrinter extends QuestionAnswerCMLAdaptor<Integer, String> {
 
@@ -23,9 +27,30 @@ public class RefinePrettyPrinter extends QuestionAnswerCMLAdaptor<Integer, Strin
 	}
 
 	@Override
-	public String caseABracketedExp(ABracketedExp node, Integer question)
+	public String caseAActionDefinition(AActionDefinition node, Integer question)
 			throws AnalysisException {
-		return "(" + node.getExpression().apply(this, question) + ")";
+		// TODO Auto-generated method stub
+		return super.caseAActionDefinition(node, question);
 	}
+
+	@Override
+	public String caseAActionProcess(AActionProcess node, Integer question)
+			throws AnalysisException {
+		return node.getAction().apply(this, question);
+	}
+
+	@Override
+	public String caseAActionStm(AActionStm node, Integer question)
+			throws AnalysisException {
+		return node.getAction().apply(this, question);
+	}
+
+	@Override
+	public String caseAAssignmentStm(AAssignmentStm node, Integer question)
+			throws AnalysisException {
+		return node.getTarget().toString() + " := " + node.getExp().toString();
+	}
+
+	
 
 }
