46a08bada74ec7f6c131ac626d66aa9c91e75866
Luis Diogo Couto <ldc@eng.au.dk>
2014-06-03 11:34:38 +0200

    Initial infrastructure for bug regression test worl/

--- core/analysis/pog/src/test/java/eu/compassresearch/pog/tests/AdHocTest.java
+++ core/analysis/pog/src/test/java/eu/compassresearch/pog/tests/AdHocTest.java
@@ -1,80 +1,158 @@
 package eu.compassresearch.pog.tests;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.node.INode;
 import org.overture.pog.pub.IProofObligation;
 import org.overture.pog.pub.IProofObligationList;
 
-import eu.compassresearch.core.analysis.pog.obligations.CmlProofObligation;
-import eu.compassresearch.core.analysis.pog.obligations.CmlProofObligationList;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import eu.compassresearch.core.analysis.pog.utility.PogPubUtil;
 import eu.compassresearch.pog.tests.utils.TestInputHelper;
+
 /**
- * Simple test class to play around with the POG without having
- * to waste time running the IDE tool.
+ * Simple test class to play around with the POG without having to waste time
+ * running the IDE tool.
+ * 
  * @author ldc
- *
+ * 
  */
 public class AdHocTest {
 
-	/**
-	 * Main test method. Runs a test on the adhoc/test.cml file. Place whatever 
-	 * model you wish to test there. That file is treated as a sandbox so
-	 * do not expect anything you place there to remain there.
-	 * @throws AnalysisException
-	 * @throws IOException
-	 */
 	@Test
-	public void testCmlPog() throws AnalysisException, IOException {
-		String file = "src/test/resources/adhoc/adhoc.cml";
-		System.out.println("Processing " + file);
-		IProofObligationList poList = buildPosFromFile(file);
+	public void quickTest() throws AnalysisException, IOException,
+			URISyntaxException {
 
-		System.out.println("\n------------------------");
-		System.out.println("POS FOR: "+file);
-				
-		for (IProofObligation po : poList) {
-			System.out.println("------------------------");	
-			StringBuilder pretty = new StringBuilder();
-			pretty.append("Full Name: " + po.getUniqueName());			
-			pretty.append("\nIsabelle Name: " + po.getIsaName());
-			pretty.append("\nPO: " + getPreamble(po));
-			pretty.append(po.getValueTree().toString());
-			System.out.println(pretty.toString());
+		// switch this flag to update a test result file
+		boolean write_result = true;
 
+		// switch this flag to print the stored results
+		boolean show_result = true;
+
+		String model = "src/test/resources/adhoc/adhoc.cml";
+		String result = "src/test/resources/adhoc/adhoc.cml.RESULT";
+
+		List<INode> ast =TestInputHelper.getAstFromName(model);
+
+		IProofObligationList ipol = PogPubUtil.generateProofObligations(ast);
+
+		System.out.println("ACTUAL POs:");
+		for (IProofObligation po : ipol) {
+			System.out.println(po.getKindString() + " / " + po.getValue());
 		}
-		System.out.println("--------DONE-----------");
+
+		if (write_result) {
+			this.update(ipol, result);
+		}
+
+		if (show_result) {
+			this.compareWithResults(ipol, result);
+		}
 
 	}
-
-	private String getPreamble(IProofObligation po)
 	
-	{
-		StringBuilder sb = new StringBuilder();
-		if (po instanceof CmlProofObligation)
-		{
-			sb.append(((CmlProofObligation) po).cmltype.toString());
+	public void compareWithResults(IProofObligationList ipol, String resultpath)
+			throws FileNotFoundException, IOException {
+
+		// read and deserialize results
+		Gson gson = new Gson();
+		String json = IOUtils.toString(new FileReader(resultpath));
+		Type datasetListType = new TypeToken<Collection<PoResult>>() {
+		}.getType();
+		List<PoResult> lpr = gson.fromJson(json, datasetListType);
+
+		System.out.println("STORED POs:");
+		for (PoResult por : lpr) {
+			System.out.println(por.getPoKind() + " / " + por.getPoExp());
 		}
-		else {
-			sb.append(po.getKind().toString());
+
+	}
+
+	private void update(IProofObligationList ipol, String resultpath)
+			throws AnalysisException, IOException, URISyntaxException {
+
+		List<PoResult> prl = new LinkedList<PoResult>();
+
+		for (IProofObligation po : ipol) {
+			prl.add(new PoResult(po.getKindString(), po.getValue()));
 		}
+
+		Gson gson = new Gson();
+		String json = gson.toJson(prl);
+
+		IOUtils.write(json, new FileOutputStream(resultpath));
 		
-		sb.append("\n");
-		return sb.toString();
+		System.out.println("\n" +resultpath + " file updated \n");
+
 	}
+	
+	
 
-	private IProofObligationList buildPosFromFile(String file) throws IOException,
-			AnalysisException {
-
-		IProofObligationList poList = new CmlProofObligationList();
-		List<INode> ast = TestInputHelper.getAstFromName(file);
-		poList = PogPubUtil.generateProofObligations(ast);
-		return poList;
-	}
-
+	/**
+//	 * Main test method. Runs a test on the adhoc/test.cml file. Place whatever
+//	 * model you wish to test there. That file is treated as a sandbox so do not
+//	 * expect anything you place there to remain there.
+//	 * 
+//	 * @throws AnalysisException
+//	 * @throws IOException
+//	 */
+//	@Test
+//	public void testCmlPog() throws AnalysisException, IOException {
+//		String file = "src/test/resources/adhoc/adhoc.cml";
+//		System.out.println("Processing " + file);
+//		IProofObligationList poList = buildPosFromFile(file);
+//
+//		System.out.println("\n------------------------");
+//		System.out.println("POS FOR: " + file);
+//
+//		for (IProofObligation po : poList) {
+//			System.out.println("------------------------");
+//			StringBuilder pretty = new StringBuilder();
+//			pretty.append("Full Name: " + po.getUniqueName());
+//			pretty.append("\nIsabelle Name: " + po.getIsaName());
+//			pretty.append("\nPO: " + getPreamble(po));
+//			pretty.append(po.getValueTree().toString());
+//			System.out.println(pretty.toString());
+//
+//		}
+//		System.out.println("--------DONE-----------");
+//
+//	}
+//
+//	private String getPreamble(IProofObligation po)
+//
+//	{
+//		StringBuilder sb = new StringBuilder();
+//		if (po instanceof CmlProofObligation) {
+//			sb.append(((CmlProofObligation) po).cmltype.toString());
+//		} else {
+//			sb.append(po.getKind().toString());
+//		}
+//
+//		sb.append("\n");
+//		return sb.toString();
+//	}
+//
+//	private IProofObligationList buildPosFromFile(String file)
+//			throws IOException, AnalysisException {
+//
+//		IProofObligationList poList = new CmlProofObligationList();
+//		List<INode> ast = TestInputHelper.getAstFromName(file);
+//		poList = PogPubUtil.generateProofObligations(ast);
+//		return poList;
+//	}
 
 }
--- core/analysis/pog/src/test/java/eu/compassresearch/pog/tests/utils/TestResultHelper.java
+++ core/analysis/pog/src/test/java/eu/compassresearch/pog/tests/utils/TestResultHelper.java
@@ -1,10 +1,20 @@
 package eu.compassresearch.pog.tests.utils;
 
+import static org.junit.Assert.fail;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.IOUtils;
+import org.overture.pog.pub.IProofObligation;
+import org.overture.pog.pub.IProofObligationList;
+
+import eu.compassresearch.pog.tests.PoResult;
 
 /**
  * Helper Class for the POG test framework. Helps generate test results.
@@ -22,6 +32,45 @@ public class TestResultHelper
 		String sResult = IOUtils.toString(new FileReader(resultPath));
 		
 		return sResult;
+	}
+	
+	public static void checkSameElements(List<PoResult> pRL,
+			IProofObligationList ipol)
+	{
+
+		List<PoResult> prl_actual = new LinkedList<PoResult>();
+		for (IProofObligation ipo : ipol)
+		{
+			prl_actual.add(new PoResult(ipo.getKindString(), ipo.getValue()));
+		}
+
+		Collection<PoResult> stored_notfound = CollectionUtils.removeAll(pRL, prl_actual);
+		Collection<PoResult> found_notstored = CollectionUtils.removeAll(prl_actual, pRL);
+
+		if (stored_notfound.isEmpty() && found_notstored.isEmpty())
+		{
+			// do nothing
+		} else
+		{
+			StringBuilder sb = new StringBuilder();
+			if (!stored_notfound.isEmpty())
+			{
+				sb.append("Expected (but not found) POS: " + "\n");
+				for (PoResult pr : stored_notfound)
+				{
+					sb.append(pr.toString() + "\n");
+				}
+			}
+			if (!found_notstored.isEmpty())
+			{
+				sb.append("Found (but not expected) POS: " + "\n");
+				for (PoResult pr : found_notstored)
+				{
+					sb.append(pr.toString() + "\n");
+				}
+			}
+			fail(sb.toString());
+		}
 	}
 
 }
