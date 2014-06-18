3cf4541bfa4ff9671823e0a7ccbfd55665f9152b
Kenneth Lausdahl <kel@iha.dk>
2014-06-18 20:11:58 +0200

    updated display function for behaviours

--- core/interpreter/src/main/utility/eu/compassresearch/core/interpreter/CmlBehaviourUtility.java
+++ core/interpreter/src/main/utility/eu/compassresearch/core/interpreter/CmlBehaviourUtility.java
@@ -17,6 +17,7 @@ import org.overture.interpreter.values.Value;
 
 import eu.compassresearch.core.interpreter.api.CmlBehaviour;
 import eu.compassresearch.core.interpreter.api.InterpreterRuntimeException;
+import eu.compassresearch.core.interpreter.runtime.DelayedWriteContext;
 
 class CmlBehaviourUtility
 {
@@ -44,12 +45,14 @@ class CmlBehaviourUtility
 		return false;
 	}
 
-	/*
+	/**
 	 * This method makes a copy of the contexts up until the ObjectContext which represents the current executing
 	 * process. So we should never go beyond an Objectcontext
 	 */
 	public static Context deepCopyProcessContext(Context from)
 	{
+//		return new DelayedWriteContext(from.assistantFactory, from.location, from.title, from);
+		
 		Context result = null;
 		// We should never copy more than the ObjectContext so If from is a
 		// ObjectContext then we stop and point to the old outer
@@ -204,5 +207,62 @@ class CmlBehaviourUtility
 		}
 
 		return dstContext;
+	}
+
+	/**
+	 * Debug method for displaying a {@link ConcreteCmlBehaviour} with its {@code next} field containing the node and
+	 * context. It also displays the two children left and right.
+	 * 
+	 * @param tabs
+	 *            initial value should be 0
+	 * @param behaviour
+	 *            the behavior to display
+	 * @return the display string. Can be ignored by the caller since it is also printed to std out.
+	 */
+	@SuppressWarnings("javadoc")
+	public static String printBehaviour(int tabs, CmlBehaviour behaviour)
+	{
+		String t = "";
+		int i = tabs;
+		while (i > 0)
+		{
+			t += "\t";
+			i--;
+		}
+
+		if (behaviour == null)
+		{
+			return (t + "null");
+		}
+		StringBuilder sb = new StringBuilder();
+		if (behaviour instanceof ConcreteCmlBehaviour)
+		{
+
+			ConcreteCmlBehaviour cb = (ConcreteCmlBehaviour) behaviour;
+
+			sb.append(t + "##############-START-" + tabs + "-###########\n");
+			if(cb.next!=null)
+			{
+			sb.append(t + "Node    = "
+					+ (cb.next.first + "").replace('\n', ' ') + "\n");
+			sb.append(t + "Context = "
+					+ (cb.next.second + "").replace('\n', ' ') + "\n");
+			sb.append(t + "State   = "
+					+ (cb.next.second.getSelf() + "").replace('\n', ' ') + "\n");
+			}
+			sb.append(t + "Pre-constructed child ctxt = "+(cb.preConstructedChildContexts+"").replace('\n', ' ')+"\n");
+			sb.append(t + "------------LEFT-----------\n");
+			sb.append(printBehaviour(tabs + 1, cb.getLeftChild()) + "\n");
+			sb.append(t + "------------RIGHT----------\n");
+			sb.append(printBehaviour(tabs + 1, cb.getRightChild()) + "\n");
+			sb.append(t + "############-END-" + tabs + "-##############\n");
+
+		}
+
+		if (tabs == 0)
+		{
+			System.out.println(sb);
+		}
+		return sb.toString();
 	}
 }
