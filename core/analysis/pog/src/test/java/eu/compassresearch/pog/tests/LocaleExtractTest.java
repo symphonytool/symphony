1babc20bba3d66b1d922d3065c7fde3b5e15b94c
Luis Diogo Couto <ldc@eng.au.dk>
2014-06-20 13:48:47 +0100

    Add locale extract test for global defition.

--- core/analysis/pog/src/test/java/eu/compassresearch/pog/tests/LocaleExtractTest.java
+++ core/analysis/pog/src/test/java/eu/compassresearch/pog/tests/LocaleExtractTest.java
@@ -6,39 +6,59 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.node.INode;
 import org.overture.pog.pub.IPogAssistantFactory;
 import org.overture.pog.pub.IProofObligation;
 import org.overture.pog.pub.IProofObligationList;
-import org.overture.pog.pub.ProofObligationGenerator;
 
 import eu.compassresearch.core.analysis.pog.utility.PogPubUtil;
 import eu.compassresearch.core.analysis.pog.visitors.CmlPogAssistantFactory;
 import eu.compassresearch.pog.tests.utils.TestInputHelper;
 
-public class LocaleExtractTest
-{
+public class LocaleExtractTest {
 
-	private final static String testmodel = "src/test/resources/basic/other/locale.cml";
-	private final static String result = "Locale";
-	IPogAssistantFactory af = new CmlPogAssistantFactory();
+	IPogAssistantFactory af;
+	
+	@Before
+	public void setup() {
+		af = new CmlPogAssistantFactory();
+	}
 
 	@Test
-	public void test_ActionProc() throws IOException, AnalysisException
-	{
+	public void test_ActionProc() throws IOException, AnalysisException {
+		String testmodel = "src/test/resources/basic/other/localeProcess.cml";
+		String result = "Locale";
+
 		List<INode> ast = TestInputHelper.getAstFromName(testmodel);
 		List<String> actual = new LinkedList<String>();
 		IProofObligationList ipol = PogPubUtil.generateProofObligations(ast);
-		for (IProofObligation ipo  : ipol)
-		{
+		for (IProofObligation ipo : ipol) {
 			actual.add(ipo.getLocale());
 		}
 		List<String> expected = new LinkedList<String>();
 		expected.add(result);
 		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	public void test_GlobalFunc() throws IOException, AnalysisException {
+		String testmodel = "src/test/resources/basic/other/localeGlobalValue.cml";
+		String result = null;
+
 		
-		
+		List<INode> ast = TestInputHelper.getAstFromName(testmodel);
+		List<String> actual = new LinkedList<String>();
+		IProofObligationList ipol = PogPubUtil.generateProofObligations(ast);
+		for (IProofObligation ipo : ipol) {
+			actual.add(ipo.getLocale());
+		}
+		List<String> expected = new LinkedList<String>();
+		expected.add(result);
+		assertEquals(expected, actual);
+
 	}
 }
