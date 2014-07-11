e6ed8f72d82dec7e3392242f196dc8b5dd77bf47
Kenneth Lausdahl <kel@iha.dk>
2014-07-11 09:47:53 +0200

    removed old hardcoded tests

--- core/s2c/src/test/java/eu/compassresearch/core/s2c/TranlationTest.java
+++ core/s2c/src/test/java/eu/compassresearch/core/s2c/TranlationTest.java
@@ -79,48 +79,6 @@ public class TranlationTest
 	
 	
 	
-//	@Test
-//	public void TranslateDwarfSimple() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException
-//	{
-//		check(translate(new File("src/test/resources/Dwarf/DwarfSimple.xmi".replace('/', File.separatorChar))));
-//	}
-//	
-//	@Test
-//	public void TranslateS2CLight() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException
-//	{
-//		check(translate(new File("src/test/resources/s2c-lite-initial-model2.xmi".replace('/', File.separatorChar))));
-//	}
-//	
-//	@Test
-//	public void TranslateS2CLightStatic() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException
-//	{
-//		check(translate(new File("src/test/resources/s2c-lite-initial-model2-static.xmi".replace('/', File.separatorChar))));
-//	}
-//	
-//	@Test
-//	public void TranslateHSUV() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException
-//	{
-//		check(translate(new File("src/test/resources/HSUV/HSUV.xmi".replace('/', File.separatorChar))));
-//	}
-	
-//	@Test
-//	public void TranslateBeoControlStreaming() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException
-//	{
-//		check(translate(new File("src/test/resources/Streaming/BeoControlStreamingSosDesignModel.xmi".replace('/', File.separatorChar))));
-//	}
-//	
-//	@Test
-//	public void TranslateBeoControlStreamingUpdated() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException
-//	{
-//		check(translate(new File("src/test/resources/Streaming/BeoControlStreamingSosDesignModel-updated.xmi".replace('/', File.separatorChar))));
-//	}
-//
-//	@Test
-//	public void TranslateInsiel() throws XPathExpressionException, ParserConfigurationException, SAXException, IOException
-//	{
-//		check(translate(new File("src/test/resources/Insiel/insiel.xmi".replace('/', File.separatorChar))));
-//	}
-//	
 	public File translate(File input) throws XPathExpressionException,
 			ParserConfigurationException, SAXException, IOException
 	{
