0fa646d09b065c12c6031dd728aa28b7b85dc90f
Alvaro Miyazawa <alvaro.miyazawa@york.ac.uk>
2014-07-29 14:39:06 +0100

    Added the copy rule for both simple action calls and parametrised actions (including val, res and vres parameters). I had to implement this as a progammatic rule because I needed access to the ast to recover the definition of the action.

--- ide/plugins/refinementtool/src/main/java/eu/compassresearch/ide/refinementtool/maude/MaudePrettyPrinter.java
+++ ide/plugins/refinementtool/src/main/java/eu/compassresearch/ide/refinementtool/maude/MaudePrettyPrinter.java
@@ -1,5 +1,11 @@
 package eu.compassresearch.ide.refinementtool.maude;
 
+import java.util.Iterator;
+
+import org.overture.ast.analysis.AnalysisException;
+import org.overture.ast.expressions.PExp;
+
+import eu.compassresearch.ast.actions.AReferenceAction;
 import eu.compassresearch.ide.refinementtool.RefinePrettyPrinter;
 
 public class MaudePrettyPrinter extends RefinePrettyPrinter {
@@ -7,6 +13,26 @@ public class MaudePrettyPrinter extends RefinePrettyPrinter {
 	public MaudePrettyPrinter() {
 		super();
 		cmlpp = new MaudePExprPrettyPrinter();
+	}
+	
+	@Override
+	public String caseAReferenceAction(AReferenceAction node, Integer question)
+			throws AnalysisException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("#ref ");
+		sb.append("#nm(\""+node.getName()+"\")");
+		if (node.getArgs().size() > 0) {
+			sb.append("(");
+			for (Iterator<PExp> it = node.getArgs().iterator(); it.hasNext(); ) {
+				PExp exp = it.next();
+				sb.append(exp.apply(cmlpp));
+				if (it.hasNext()) {
+					sb.append(", ");
+				}
+			}
+			sb.append(")");
+		}
+		return sb.toString();
 	}
 
 }
