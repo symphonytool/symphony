955a47d279082eaf1dbf457ebd352c121688e6ad
Claus Nielsen <clausbn@iha.dk>
2014-08-06 15:21:11 +0200

    added serial UID

--- ide/plugins/collaboration/src/main/java/eu/compassresearch/ide/collaboration/communication/messages/LeftCollaborationMessage.java
+++ ide/plugins/collaboration/src/main/java/eu/compassresearch/ide/collaboration/communication/messages/LeftCollaborationMessage.java
@@ -4,6 +4,8 @@ import org.eclipse.ecf.core.identity.ID;
 
 public class LeftCollaborationMessage extends BaseMessage
 {
+	private static final long serialVersionUID = 6136617078011072335L;
+
 	public LeftCollaborationMessage(ID sender, String collabProjectID)
 	{
 		super(sender, collabProjectID);
