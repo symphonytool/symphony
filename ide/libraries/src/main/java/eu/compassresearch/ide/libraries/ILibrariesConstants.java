006f948efc31961037f4c3c35b3ca471e67d2aab
Kenneth Lausdahl <kel@iha.dk>
2014-08-27 11:25:31 +0200

    added initial library support based on DESTECS

---
+++ ide/libraries/src/main/java/eu/compassresearch/ide/libraries/ILibrariesConstants.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 DESTECS Team and others.
+ *
+ * DESTECS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DESTECS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DESTECS.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ * The DESTECS web-site: http://destecs.org/
+ *******************************************************************************/
+package eu.compassresearch.ide.libraries;
+
+public interface ILibrariesConstants
+{
+public static final String PLUGIN_ID="eu.compassresearch.ide.libraries";
+}
